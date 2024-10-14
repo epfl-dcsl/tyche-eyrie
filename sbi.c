5c925a06e2b11a2bdb0fbea98f1916c787eb5864
CharlyCst <charly.castes@epfl.ch>
2024-10-14 12:44:43 +0200

    Initial patch te remove most sbi calls

--- sbi.c
+++ sbi.c
@@ -1,4 +1,5 @@
 #include "sbi.h"
+#include "printf.h"
 
 #include "vm_defs.h"
 
@@ -26,6 +27,12 @@
 #define SBI_CALL_3(___ext, ___which, ___arg0, ___arg1, ___arg2) \
   SBI_CALL(___ext, ___which, ___arg0, ___arg1, ___arg2)
 
+#define SBI_ERR_SM_ENCLAVE_SUCCESS      0
+#define SBI_ERR_SM_NOT_IMPLEMENTED 100100
+
+// Eyrie uses the old timer extension par default, switch to the new one that Tyche supports
+#define SBI_EXT_TIME 0x54494D45
+
 void
 sbi_putchar(char character) {
   SBI_CALL_1(SBI_CONSOLE_PUTCHAR, 0, character);
@@ -34,45 +41,57 @@ sbi_putchar(char character) {
 void
 sbi_set_timer(uint64_t stime_value) {
 #if __riscv_xlen == 32
-  SBI_CALL_2(SBI_SET_TIMER, 0, stime_value, stime_value >> 32);
+  SBI_CALL_2(SBI_EXT_TIME, 0, stime_value, stime_value >> 32);
 #else
-  SBI_CALL_1(SBI_SET_TIMER, 0, stime_value);
+  SBI_CALL_1(SBI_EXT_TIME, 0, stime_value);
 #endif
 }
 
 uintptr_t
 sbi_stop_enclave(uint64_t request) {
+  printf("[CC] sbi stop enclave\n");
   return SBI_CALL_1(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, SBI_SM_STOP_ENCLAVE, request);
 }
 
 void
 sbi_exit_enclave(uint64_t retval) {
+  printf("[CC] sbi exit enclave\n");
   SBI_CALL_1(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, SBI_SM_EXIT_ENCLAVE, retval);
 }
 
 uintptr_t
 sbi_random() {
-  return SBI_CALL_0(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, SBI_SM_RANDOM);
+  printf("[CC] sbi call random\n");
+  return 0;
+  // return SBI_CALL_0(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, SBI_SM_RANDOM);
 }
 
 uintptr_t
 sbi_query_multimem() {
-  return SBI_CALL_2(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE,
-      SBI_SM_CALL_PLUGIN, SM_MULTIMEM_PLUGIN_ID, SM_MULTIMEM_CALL_GET_SIZE);
+  printf("[CC] sbi query multimem\n");
+  return 0; //SBI_ERR_SM_NOT_IMPLEMENTED;
+  /* return SBI_CALL_2(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, */
+  /*     SBI_SM_CALL_PLUGIN, SM_MULTIMEM_PLUGIN_ID, SM_MULTIMEM_CALL_GET_SIZE); */
 }
 
 uintptr_t
 sbi_query_multimem_addr() {
-  return SBI_CALL_2(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE,
-      SBI_SM_CALL_PLUGIN, SM_MULTIMEM_PLUGIN_ID, SM_MULTIMEM_CALL_GET_ADDR);
+  printf("[CC] sbi query multimem addr\n");
+  return 0; //SBI_ERR_SM_NOT_IMPLEMENTED;
+  /* return SBI_CALL_2(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, */
+  /*     SBI_SM_CALL_PLUGIN, SM_MULTIMEM_PLUGIN_ID, SM_MULTIMEM_CALL_GET_ADDR); */
 }
 
 uintptr_t
 sbi_attest_enclave(void* report, void* buf, uintptr_t len) {
-  return SBI_CALL_3(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, SBI_SM_ATTEST_ENCLAVE, report, buf, len);
+  printf("[CC] sbi attest enclave\n");
+  return SBI_ERR_SM_ENCLAVE_SUCCESS;
+  /* return SBI_CALL_3(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, SBI_SM_ATTEST_ENCLAVE, report, buf, len); */
 }
 
 uintptr_t
 sbi_get_sealing_key(uintptr_t key_struct, uintptr_t key_ident, uintptr_t len) {
-  return SBI_CALL_3(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, SBI_SM_GET_SEALING_KEY, key_struct, key_ident, len);
+  printf("[CC] sbi get sealing key\n");
+  return SBI_ERR_SM_ENCLAVE_SUCCESS;
+  /* return SBI_CALL_3(SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE, SBI_SM_GET_SEALING_KEY, key_struct, key_ident, len); */
 }
